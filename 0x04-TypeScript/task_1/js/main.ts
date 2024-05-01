fe0f40b587dbab02870ba196c7704b49e242e7c5
yasmen <yasmen.khaled2000@gmail.com>
2024-05-01 22:53:27 +0200

    all done

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,60 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+
+
+// console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+
+
+/* eslint-disable @typescript-eslint/class-name-casing */
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+/* eslint-enable @typescript-eslint/class-name-casing */
+export const printTeacher: printTeacherFunction = function (
+  firstName: string,
+  lastName: string
+): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// console.log(printTeacher("John", "Doe"));
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
